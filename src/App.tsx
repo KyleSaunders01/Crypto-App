5cdb81bedc516612f3ed521d30a9a61c150647a1
Kyle Saunders <kylesa14@gmail.com>
2024-10-22 22:20:08 +0200

    Test deploy workflow

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,42 @@
-import { HashRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import { Layout, Typography, Space, Avatar } from 'antd';
-import './App.css';
+import { Routes, Route, Link } from 'react-router-dom';
+import {Layout, Typography, Space, Avatar} from 'antd';
+import './App.css'
 import { Navbar, Exchanges, HomePage, Cryptocurrencies, CryptoDetails } from './components';
-import icon from '../public/cryptoco.png';
-
+import icon from "../public/cryptoco.png";
 function App() {
-    return (
-        <Router basename="/Crypto-App"> {/* Add basename to handle the subdirectory */}
-            <div className="app">
-                <div className="navbar">
-                    <Navbar />
+
+  return (
+    <div className="app">
+        <div className="navbar">
+            <Navbar/>
+        </div>
+        <div className="main">
+            <Layout>
+                <div className="routes">
+                    <Routes>
+                        <Route path="/" element={<HomePage/>}/>
+                        <Route path="/exchanges" element={<Exchanges/>}/>
+                        <Route path="/cryptocurrencies" element={<Cryptocurrencies/>}/>
+                        <Route path="/crypto/:coinId" element={<CryptoDetails/>}/>
+                    </Routes>
                 </div>
-                <div className="main">
-                    <Layout>
-                        <div className="routes">
-                            <Routes>
-                                <Route path="/" element={<HomePage />} />
-                                <Route path="/exchanges" element={<Exchanges />} />
-                                <Route path="/cryptocurrencies" element={<Cryptocurrencies />} />
-                                <Route path="/crypto/:coinId" element={<CryptoDetails />} />
-                            </Routes>
-                        </div>
-                    </Layout>
-                    <div className="footer">
-                        <Avatar src={icon} size="small" />
-                        <Typography.Title level={5} style={{ color: 'white', textAlign: 'center' }}>
-                            Cryptoco <br />
-                        </Typography.Title>
-                        <Space>
-                            <Link to="/">Home</Link>
-                            <Link to="/cryptocurrencies">Cryptocurrencies</Link>
-                            <Link to="/exchanges">Exchanges</Link>
-                        </Space>
-                    </div>
-                </div>
+            </Layout>
+            <div className="footer">
+
+                <Avatar src={icon} size="small"/>
+                <Typography.Title level={5} style={{color: 'white', textAlign: 'center'}}>
+                    Cryptoco <br/>
+                </Typography.Title>
+                <Space>
+                    <Link to="/">Home</Link>
+                    <Link to="/cryptocurrencies">Cryptocurrencies</Link>
+                    <Link to="/exchanges">Exchanges</Link>
+                </Space>
             </div>
-        </Router>
-    );
+        </div>
+
+    </div>
+  )
 }
 
-export default App;
+export default App
