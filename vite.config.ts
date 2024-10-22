6d8e5457b7a6e93066e08dcc7cb64a3c8dfaa936
Kyle Saunders <kylesa14@gmail.com>
2024-10-22 21:20:27 +0200

    Clean up

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import { ghPages } from 'vite-plugin-gh-pages';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), ghPages()],
-  base: '/<Crypto-App>/', // Replace <repository-name> with your GitHub repo name
-});
+  plugins: [react()],
+  base: '/Crypto-App/', // Set the base to your GitHub repository name
+})
+
